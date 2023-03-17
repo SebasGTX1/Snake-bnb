24b758383f682fd2fad375da110d925b27b88aa0
SebasGTX1 <sebas-0202@hotmail.es>
2023-03-16 22:23:02 -0500

    Initial data from the mongodb course

---
+++ src/program.py
@@ -0,0 +1,65 @@
+from colorama import Fore
+import program_guests
+import program_hosts
+
+
+def main():
+    # TODO: Setup mongoengine global values
+
+    print_header()
+
+    try:
+        while True:
+            if find_user_intent() == 'book':
+                program_guests.run()
+            else:
+                program_hosts.run()
+    except KeyboardInterrupt:
+        return
+
+
+def print_header():
+    snake = \
+        """
+    Y
+  .-^-.
+ /     \      .- ~ ~ -.
+()     ()    /   _ _   `.                     _ _ _
+ \_   _/    /  /     \   \                . ~  _ _  ~ .
+   | |     /  /       \   \             .' .~       ~-. `.
+   | |    /  /         )   )           /  /             `.`.
+   \ \_ _/  /         /   /           /  /                `'
+    \_ _ _.'         /   /           (  (
+                    /   /             \  \\
+                   /   /               \  \\
+                  /   /                 )  )
+                 (   (                 /  /
+                  `.  `.             .'  /
+                    `.   ~ - - - - ~   .'
+                       ~ . _ _ _ _ . ~
+                         
+         https://www.asciiart.eu/animals/reptiles/snakes
+                         """
+
+    print(Fore.WHITE + '****************  SNAKE BnB  ****************')
+    print(Fore.GREEN + snake)
+    print(Fore.WHITE + '*********************************************')
+    print()
+    print("Welcome to Snake BnB!")
+    print("Why are you here?")
+    print()
+
+
+def find_user_intent():
+    print("[g] Book a cage for your snake")
+    print("[h] Offer extra cage space")
+    print()
+    choice = input("Are you a [g]uest or [h]ost? ")
+    if choice == 'h':
+        return 'offer'
+
+    return 'book'
+
+
+if __name__ == '__main__':
+    main()
